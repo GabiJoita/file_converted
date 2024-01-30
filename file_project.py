7573e0ab026d40048ab6a06f1cab0fc796d3a6f2
GabiJoita <GabiJoita191@gmail.com>
2024-01-30 10:59:11 +0200

    modified file_project.py

--- file_project.py
+++ file_project.py
@@ -4,12 +4,25 @@ def open_excel_to_csv(file_name_excel:str, file_name_csv:str):
     """open excel file"""
     content = pd.read_excel(file_name_excel)
     content.to_csv(file_name_csv, index=False)
-    print(f'{file_name_excel} converted in {file_name_csv}')
+    return content
 
+def open_file_csv(file_name_csv:str):
+    """open csv file"""
+    with open (file_name_csv, 'r') as rd:
+        content = rd.read() 
+    return content
+
+def open_file_txt(file_name_txt:str):
+    """open the previews files in txt"""
+    with open(file_name_txt, 'w' ) as fr:
+        fr.write(open_file_csv(file_name_csv))
+        
 
 if __name__ == "__main__":
     file_name_excel = 'users.xlsx'
     file_name_csv = 'users.csv'
+    file_name_txt = 'users.txt'
     open_excel_to_csv(file_name_excel, file_name_csv)
-
+    open_file_csv(file_name_csv)
+    open_file_txt(file_name_txt)
 
